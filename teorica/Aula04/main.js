91983bfae1ff76d3284a0f8b056bcca2ce3c177e
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-09-20 19:00:41 -0300

    .

---
+++ teorica/Aula04/main.js
@@ -0,0 +1,4 @@
+import { printFrutas } from "./prints"
+
+printFrutas('Melancia')
+
---
+++ teorica/Aula04/prints.js
@@ -0,0 +1,12 @@
+export function printFrutas(conteudo){
+    let texto = conteudo 
+    let linha = ''
+    for(let i=0; i<=texto.length;i++){
+        linha = linha+'-'
+    }
+
+console.log(`+${linha}+`)
+console.log(`|${texto}|`)
+console.log(`+${linha}+`)
+}
+
